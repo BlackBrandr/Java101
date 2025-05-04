c51411590830926d2bc24d0e799a85f580c13fc1
Burak Karataş <93159179+BlackBrandr@users.noreply.github.com>
2025-05-04 18:40:23 +0300

    Add files via upload

---
+++ Exponent_Calculator.java
@@ -0,0 +1,29 @@
+package Loops;
+
+import java.util.Scanner;
+
+/**
+ * Bu sınıf, kullanıcıdan alınan taban ve üs değerlerine göre üs alma işlemi yapar
+ */
+public class Exponent_Calculator {
+    public static void main(String[] args) {
+        // Değişkenlerin tanımlanması
+        int number, exponent, result = 1;
+
+        // Kullanıcıdan taban ve üs değerlerini alma
+        Scanner input = new Scanner(System.in);
+        System.out.println("Taban giriniz: ");
+        number = input.nextInt();
+
+        System.out.println("Üs giriniz: ");
+        exponent = input.nextInt();
+
+        // Üs alma işlemini gerçekleştirme
+        for (int i = 1; i <= exponent; i++) {
+            result *= number;
+        }
+
+        // Sonucu ekrana yazdırma
+        System.out.println(number + "^" + exponent + " = " + result);
+    }
+}
