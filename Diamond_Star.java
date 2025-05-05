5fd28dcdc20fd5f5ecc0b1e7921d3b2f6a21742d
Burak Karataş <93159179+BlackBrandr@users.noreply.github.com>
2025-05-05 11:11:53 +0300

    Add files via upload

---
+++ Diamond_Star.java
@@ -0,0 +1,39 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class Diamond_Star{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Elmasın yüksekliğini giriniz (tek sayı): ");
+        int n = input.nextInt();
+
+        // Üst kısım
+        for (int i = 0; i <= n/2; i++) {
+            // Boşlukları yazdır
+            for (int j = 0; j < (n/2)-i; j++) {
+                System.out.print(" ");
+            }
+            // Yıldızları yazdır
+            for (int k = 0; k < 2*i+1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Alt kısım
+        for (int i = n/2-1; i >= 0; i--) {
+            // Boşlukları yazdır
+            for (int j = 0; j < (n/2)-i; j++) {
+                System.out.print(" ");
+            }
+            // Yıldızları yazdır
+            for (int k = 0; k < 2*i+1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        input.close();
+    }
+}
