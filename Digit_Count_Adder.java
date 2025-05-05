98a9252efb2ab12a04d6905299bdc3a2cfba1687
Burak Karataş <93159179+BlackBrandr@users.noreply.github.com>
2025-05-05 11:00:27 +0300

    Add files via upload

---
+++ Digit_Count_Adder.java
@@ -0,0 +1,31 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class Digit_Count_Adder {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı Giriniz :");
+        int number = input.nextInt();
+        int basNumber = 0;
+        int tempNumber = number;
+        int basValue;
+        int result = 0;
+
+        while (tempNumber != 0) {
+            tempNumber /= 10;
+            basNumber++;
+        }
+
+        tempNumber = number;
+
+        while (tempNumber != 0) {
+            basValue = tempNumber % 10;
+
+            result += basValue;
+            tempNumber /= 10;
+        }
+        System.out.println("Result: " + result);
+    }
+}
