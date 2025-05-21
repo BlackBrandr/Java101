e98685dcd1c7c140058199c2ef10ed420ce85483
Burak Karataş <burakkaratas2001@gmail.com>
2025-05-21 23:09:35 +0300

    Code is running

--- Arrays/Number_Sorting.java
+++ Arrays/Number_Sorting.java
@@ -1,4 +1,25 @@
 package Methods.Arrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Number_Sorting {
+    public static void main(String[] args) {
+
+        System.out.print("Lenght of the array:");
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter the number " + (i+1) + ": ");
+            numbers[i] = input.nextInt();
+        }
+
+        Arrays.sort(numbers);
+
+        System.out.println("Sorted numbers: ");
+        System.out.println(Arrays.toString(numbers));
+
+    }
 }
